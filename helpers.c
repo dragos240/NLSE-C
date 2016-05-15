503a06e74ddc82260c63edf7bf9196713c872a36
Harley Amundsen <dragos240alt@gmail.com>
2016-05-15 15:35:19 -0400

    Program now accounts for endianness

--- helpers.c
+++ helpers.c
@@ -12,9 +12,14 @@ void printchars(char* start, char* str, int len){
 
 unsigned short toUInt16(unsigned char* data, int location){
 	unsigned short result;
-
-	result = (data[location + 1] << 8) |
-			 (data[location + 0] << 0);
+	int n = 1;
+	
+	if(*(char *)&n == 1)
+		result = (data[location + 1] << 8) |
+				 (data[location + 0] << 0);
+	else
+		result = (data[location + 0] << 8) |
+				 (data[location + 1] << 0);
 
 	return result;
 
@@ -22,11 +27,18 @@ unsigned short toUInt16(unsigned char* data, int location){
 
 unsigned int toUInt32(unsigned char* data, int location){
 	unsigned int result;
+	int n = 1;
 
-	result = (data[location + 3] << 24) |
-			 (data[location + 2] << 16) |
-			 (data[location + 1] << 8) |
-			 (data[location + 0] << 0);
+	if(*(char *)&n == 1)
+		result = (data[location + 3] << 24) |
+				 (data[location + 2] << 16) |
+				 (data[location + 1] << 8) |
+				 (data[location + 0] << 0);
+	else
+		result = (data[location + 0] << 24) |
+				 (data[location + 1] << 16) |
+				 (data[location + 2] << 8) |
+				 (data[location + 3] << 0);
 
 	return result;
 }
--- helpers.h
+++ helpers.h
@@ -1,3 +1,6 @@
+#ifndef HELPERS_H
+#define HELPERS_H
 void printchars(char* start, char* str, int len);
 unsigned short toUInt16(unsigned char* data, int location);
 unsigned int toUInt32(unsigned char* data, int location);
+#endif
